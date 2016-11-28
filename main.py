7c894bc6c60c7326d59a7519d3645a663e857f28
AhmedAtef07 <ahmedatef07@hotmail.com>
2016-11-28 05:14:31 +0200

    Add dedicated mode for concealing and extracting some image from another.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 import cv2
 import numpy as np
 
+COLOR_LAYER = 0
 
 def main():
     parser = argparse.ArgumentParser(prog="Image Steganography",
@@ -13,6 +14,11 @@ def main():
                         help='target image path to play with')
 
     hide_or_extract_group = parser.add_mutually_exclusive_group(required=True)
+    hide_or_extract_group.add_argument('-ce', '--concealandextract',
+                                       nargs=1,
+                                       type=argparse.FileType(),
+                                       help='conceal an image within the passed image (takes another image path)'
+                                            'then extract it back while previewing all the changes')
     hide_or_extract_group.add_argument('-c', '--conceal',
                                        nargs=1,
                                        type=argparse.FileType(),
@@ -24,28 +30,46 @@ def main():
     args = parser.parse_args()
     # Validate the given path points to an image
 
+    host_img_path = args.image.name
+
     if args.conceal:
-        host_image_path = args.image.name
-        message_image_path = args.conceal[0].name  # Validate this path
-        conceal_image(host_image_path, message_image_path, False)
+        message_img_path = args.conceal[0].name  # Validate this path
+        conceal_image(host_img_path, message_img_path, False)
+    elif args.extract:
+        extract(host_img_path)
+    elif args.concealandextract:
+        message_img_path = args.concealandextract[0].name  # Validate this path
+        conceal_and_extract_image(host_img_path, message_img_path, True)
 
 
-def conceal_image(host_image_path, message_image_path, preview):
+def conceal_and_extract_image(host_img_path, message_img_path, preview):
+    concealed_img = conceal_image(host_img_path, message_img_path, preview)
+
+    extracted_img = _extract(concealed_img, COLOR_LAYER)
+    _preview_image("Extracted Image", extracted_img)
+
+
+def conceal_image(host_img_path, message_img_path, preview):
     # Validate that the message image can be concealed within the host image.
     # Shape (dimensions) of host image should be larger, otherwise crop message image.
-    message_img = convert_to_binary_image(message_image_path, preview)
-    host_img = cv2.imread(host_image_path)
+    message_img = convert_to_binary_image(message_img_path, preview)
+    host_img = cv2.imread(host_img_path)
 
     _preview_image("Message Image", message_img, keep_open=True)
     _preview_image("Carrier Host Image", host_img, keep_open=True)
 
-    _conceal(host_img, message_img)
+    concealed_img = _conceal(host_img, message_img, COLOR_LAYER)
+    _preview_image("Concealed Image", concealed_img)
+
+    return concealed_img
 
 
-def _conceal(host_img_array, message_img_array):
-    """ Conceal 1's as even red pixels. """
+def _conceal(host_img_array, message_img_array, layer):
+    """ Conceal 1's as even red pixels.
+        layer: stands for which color to manipulate, RGBA:
+            0: for changing the red layer. (1, green), (2, blue), (3, alpha)
+    """
     # Convert array of int to array of boolean.
-    layer = 0  # For red.
 
     message_img_array_mask = message_img_array == 0
 
@@ -64,8 +88,19 @@ def _conceal(host_img_array, message_img_array):
                 if pixel[layer] % 2 == 0:
                     pixel[layer] += 1
 
-    _preview_image("Output Image", host_img_array, keep_open=True)
+    return host_img_array
 
+
+def extract(host_img_path):
+    host_img = cv2.imread(host_img_path)
+
+    extracted_img = _extract(host_img, COLOR_LAYER)
+    _preview_image("Extracted Image", extracted_img)
+
+    return extracted_img
+
+
+def _extract(host_img_array, layer):
     extracted_img = np.zeros(host_img_array.shape[:2], dtype=np.uint8)
 
     for r, row in enumerate(host_img_array):
@@ -73,14 +108,14 @@ def _conceal(host_img_array, message_img_array):
             if pixel[layer] % 2 != 0:
                 extracted_img[r][c] = 255
 
-    _preview_image("Extracted Image", extracted_img)
+    return extracted_img
 
 
-def convert_to_binary_image(image_path, preview):
-    img = cv2.imread(image_path)
+def convert_to_binary_image(img_path, preview):
+    img = cv2.imread(img_path)
     if preview: _preview_image("Original Message Image", img, keep_open=True)
 
-    img_gray = cv2.imread(image_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    img_gray = cv2.imread(img_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
     if preview: _preview_image("Gray Scale Message Image", img_gray, keep_open=True)
 
     (thresh, img_bw) = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
